ba58be91c4bc3c3551b8a27635a572864647f9eb
Martin Quevedo <tinchoque@yahoo.com.ar>
2020-06-30 00:30:33 -0300

    a

---
+++ src/app/models/postulantes.ts
@@ -0,0 +1,11 @@
+export class postulantes{
+  constructor(
+    public id: number,
+    public nombre: string,
+    public apellido: string,
+    public email: string,
+    public password: string,
+    public foto: string,
+    public video: string,
+  ){}
+}
